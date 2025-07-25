fdb649c23185c8cf843f25c79f9703dabdbcdcb5
Vladislav Aristov <167144105+Makotra@users.noreply.github.com>
2025-07-26 00:21:37 +0300

    Add files via upload

---
+++ proj/levels/small cloud 3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="small cloud 3" tilewidth="16" tileheight="16" tilecount="16" columns="8">
+ <image source="Small Cloud 3.png" width="140" height="39"/>
+</tileset>
